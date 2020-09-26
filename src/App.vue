974e2455d089b19230bd2ff3c90d013aa2ee4ec8
SaurabhDhakate <69841290+SaurabhDhakate@users.noreply.github.com>
2020-09-26 22:19:42 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header 
+    :message="name"
+    />
+    <Image />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
+import Image from "./components/Image.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:"app",
+  components:{
+    Header,
+    Image
+  },
+  data(){
+    return{
+      name:"Saurabh"
+    }
   }
 }
 </script>
@@ -21,6 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
